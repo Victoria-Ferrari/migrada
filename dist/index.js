8aa7c15a725d0157fb7f933158e1218aa510540a
isabellabagnulo <77104271+isabellabagnulo@users.noreply.github.com>
2022-05-20 15:37:21 +0200

    upload

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,27 @@
-// Listen for click on the document
-document.addEventListener("click", function (event) {
-  //Bail if our clicked element doesn't have the class
-  if (!event.target.classList.contains("accordion-toggle")) return
-  // Get the target content
-  var content = document.querySelector(event.target.hash)
-  if (!content) return
-  // Prevent default link behavior
-  event.preventDefault()
-  // If the content is already expanded, collapse it and quit
-  if (content.classList.contains("active")) {
-    content.classList.remove("active")
-    return
-  }
-  // Get all open accordion content, loop through it, and close it
-  var accordions = document.querySelectorAll(
-    ".accordion-content-container.active"
-  )
-  for (var i = 0; i < accordions.length; i++) {
-    accordions[i].classList.remove("active")
-  }
-  // Toggle our content
-  content.classList.toggle("active")
+const accordionHeaders = Array.from(
+  document.querySelectorAll(".accordion-toggle")
+)
+const accordionContents = Array.from(
+  document.querySelectorAll(".accordion-content-container")
+)
+
+accordionHeaders.forEach((header) => {
+  header.addEventListener("click", (event) => {
+    event.preventDefault()
+    const accordionContent = document.querySelector(event.target.hash)
+
+    accordionContents.forEach((contentEl) => {
+      if (accordionContent === contentEl) return
+      contentEl.classList.remove("active")
+    })
+    accordionHeaders.forEach((headerEl) => {
+      if (header === headerEl) return
+      headerEl.classList.remove("active")
+    })
+
+    header.classList.toggle("active")
+    accordionContent.classList.toggle("active")
+  })
 })
+
+document.addEventListener("click", () => {})
