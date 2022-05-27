ca4bfff5075a8d8edcfe5cc16b18e62a4033ebf9
isabellabagnulo <77104271+isabellabagnulo@users.noreply.github.com>
2022-05-27 17:01:20 +0200

    upload

--- dist/index.js
+++ dist/index.js
@@ -4,23 +4,24 @@ const accordionHeaders = Array.from(
 const accordionContents = Array.from(
   document.querySelectorAll(".accordion-content-container")
 )
+const days = Array.from(document.querySelectorAll(".giorno"))
 
-const novegiugno = document.querySelector("#novegiugno")
-const diecigiugno = document.querySelector("#diecigiugno")
-const undicigiugno = document.querySelector("#undicigiugno")
+// const novegiugno = document.querySelector("#novegiugno")
+// const diecigiugno = document.querySelector("#diecigiugno")
+// const undicigiugno = document.querySelector("#undicigiugno")
 
-novegiugno.addEventListener("click", () => {
-  document.querySelector("#content-1").classList.toggle("active")
-  document.querySelector("#header-1").classList.toggle("active")
-})
-diecigiugno.addEventListener("click", () => {
-  document.querySelector("#content-2").classList.toggle("active")
-  document.querySelector("#header-2").classList.toggle("active")
-})
-undicigiugno.addEventListener("click", () => {
-  document.querySelector("#content-3").classList.toggle("active")
-  document.querySelector("#header-3").classList.toggle("active")
-})
+// novegiugno.addEventListener("click", () => {
+//   document.querySelector("#content-1").classList.toggle("active")
+//   document.querySelector("#header-1").classList.toggle("active")
+// })
+// diecigiugno.addEventListener("click", () => {
+//   document.querySelector("#content-2").classList.toggle("active")
+//   document.querySelector("#header-2").classList.toggle("active")
+// })
+// undicigiugno.addEventListener("click", () => {
+//   document.querySelector("#content-3").classList.toggle("active")
+//   document.querySelector("#header-3").classList.toggle("active")
+// })
 
 accordionHeaders.forEach((header) => {
   header.addEventListener("click", (event) => {
@@ -37,6 +38,29 @@ accordionHeaders.forEach((header) => {
     })
 
     header.classList.toggle("active")
+    accordionContent.classList.toggle("active")
+  })
+})
+
+days.forEach((day) => {
+  day.addEventListener("click", (event) => {
+    const accordionContent = document.querySelector(
+      event.target.getAttribute("data-content-id")
+    )
+    const accordionHeader = document.querySelector(
+      event.target.getAttribute("data-header-id")
+    )
+
+    accordionContents.forEach((contentEl) => {
+      if (accordionContent === contentEl) return
+      contentEl.classList.remove("active")
+    })
+    accordionHeaders.forEach((headerEl) => {
+      if (accordionHeader === headerEl) return
+      headerEl.classList.remove("active")
+    })
+
+    accordionHeader.classList.toggle("active")
     accordionContent.classList.toggle("active")
   })
 })
